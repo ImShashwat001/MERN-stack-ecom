36d30f627b766b79a4d22f8bcce1f3ff7e5f65ee
Shashwat <shashwatpanda4@gmail.com>
2023-07-22 22:09:30 +0530

    jsonwebtoken added

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 
-const router = express.Router()
+const router = express.Router();
 const { signout, signup } = require("../controllers/auth");
 const { check } = require('express-validator');
 

3963d91bbc0540246a021c05c5849e28b14d15ba
Shashwat <shashwatpanda4@gmail.com>
2023-07-25 07:17:09 +0530

    signin controller added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const {check, validationResult } = require("express-validator");
-const User = require("../models/user")
+const User = require("../models/user");
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
 
 exports.signup = async (req, res) => {
@@ -30,6 +32,41 @@ exports.signup = async (req, res) => {
     };
 }
 
+exports.signin = (req, res) => {
+
+    // destructuring req.body to get email and password
+    const {email, password } = req.body;
+
+    if(!errors.isEmpty()) {
+        return res.status(422).json({
+            error:  errors.array()[0].msg
+        })
+    }
+
+    User.findOne({ email }, (err, user) => {
+        if(err) {
+            res.statys(400).json({
+                error: "USER email does not exist"
+            })
+        }
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error: "Email and password do not match"
+            })
+        }
+        //token
+
+        const token = jwt.signin({_id: user._id}, process.env.SECRET)
+
+        //put token in cookie
+        res.cookie("token, token, {expire: new Date() + 120 }");
+
+        //send response to front end
+        const {_id, name, email, role} = user;
+        return res.json({token, user: {_id, name, email, role}})
+
+    })
+}
 
 exports.signout = (req, res) => {
     res.json({
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 
 const router = express.Router();
-const { signout, signup } = require("../controllers/auth");
+const { signout, signup, signin } = require("../controllers/auth");
 const { check } = require('express-validator');
 
 
@@ -14,6 +14,16 @@ router.post("/signup",[
     check("password", "password should be at least 3 chr").isLength({ min: 3 })
 
 ], signup);
+
+router.post("/signin",[
+
+    check("email", "email is required").isEmail(),
+    check("password", "password field is required").isLength({ min: 1 })
+
+], signin);
+
+
+
 router.get("/signout", signout);
 
 module.exports = router;

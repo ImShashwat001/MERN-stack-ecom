338f4c351b6558e1a5845e4962517addb1167195
Shashwat <shashwatpanda4@gmail.com>
2023-08-06 23:04:34 +0530

    facing mongoose callback error after sending GET request, unsolved

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,20 @@
 const User = require("../models/user");
 
 exports.getUserById = (req, res, next, id) => {
-    User.findById(id).exec(() => {
+    User.findById(id).exec((err, user) => {
         if (err || !user) {
             return res.status(400).json({
                 error: "No user was found in DB"
             })
         }
+        // try { (err || !user)
+        //     return res.status(400).json({
+        //         error: "No user was found in DB"
+        //     })
+
+        // } catch {
+        //     	console.log(error)
+        // }
         req.profile = user;
         next();
     })
--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,11 @@
-const User = require("../models/user");
+const express = require('express');
+const router = express.Router();
+
+const {getUserById, getUser} = require("../controllers/user");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+
+router.param("user_id", getUserById);
+
+router.get("/user/:user_id",isSignedIn,isAuthenticated, getUser);
+
+module.exports = router;
